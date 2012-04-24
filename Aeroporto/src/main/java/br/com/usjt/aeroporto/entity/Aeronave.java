07e49faee437ff5d235ce40bc2abc3a814a8842f
Thiago Souza Cardoso <kamui.thiago@gmail.com>
2012-04-24 01:43:05 -0300

    adicionado Implementaçao de toString, equals e hashCode

--- Aeroporto/src/main/java/br/com/usjt/aeroporto/entity/Aeronave.java
+++ Aeroporto/src/main/java/br/com/usjt/aeroporto/entity/Aeronave.java
@@ -88,4 +88,83 @@ public class Aeronave implements Serializable {
 		this.listVoos = listVoos;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((listVoos == null) ? 0 : listVoos.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime
+				* result
+				+ ((quantidadeAssentos == null) ? 0 : quantidadeAssentos
+						.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Aeronave)) {
+			return false;
+		}
+		Aeronave other = (Aeronave) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (listVoos == null) {
+			if (other.listVoos != null) {
+				return false;
+			}
+		} else if (!listVoos.equals(other.listVoos)) {
+			return false;
+		}
+		if (nome == null) {
+			if (other.nome != null) {
+				return false;
+			}
+		} else if (!nome.equals(other.nome)) {
+			return false;
+		}
+		if (quantidadeAssentos == null) {
+			if (other.quantidadeAssentos != null) {
+				return false;
+			}
+		} else if (!quantidadeAssentos.equals(other.quantidadeAssentos)) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Aeronave [id=" + id + ", nome=" + nome
+				+ ", quantidadeAssentos=" + quantidadeAssentos + ", listVoos="
+				+ listVoos + "]";
+	}
 }
